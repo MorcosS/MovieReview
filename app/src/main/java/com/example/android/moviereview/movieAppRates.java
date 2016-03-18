dc1221c674f17ffa35a7f5cb48de689600414f11
MorcosS <morkos.said@gmail.com>
2016-03-18 21:13:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/moviereview/movieAppRates.java
@@ -0,0 +1,13 @@
+package com.example.android.moviereview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class movieAppRates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_app_rates);
+    }
+}
